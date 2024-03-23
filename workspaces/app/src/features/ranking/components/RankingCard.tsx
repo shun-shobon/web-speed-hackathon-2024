376e002855c1bab22f3fd268fdca428db246528b
NISHIZAWA Shuntaro <me@s2n.tech>
2024-03-23 15:40:23 +0900

    アイコンライブラリのツリーシェイキングの有効化

--- workspaces/app/src/features/ranking/components/RankingCard.tsx
+++ workspaces/app/src/features/ranking/components/RankingCard.tsx
@@ -1,7 +1,7 @@
+import { NavigateNext } from '@mui/icons-material';
 import { Suspense } from 'react';
 import styled from 'styled-components';
 
-import { SvgIcon } from '../../../features/icons/components/SvgIcon';
 import { Box } from '../../../foundation/components/Box';
 import { Flex } from '../../../foundation/components/Flex';
 import { Image } from '../../../foundation/components/Image';
@@ -12,7 +12,6 @@ import { Text } from '../../../foundation/components/Text';
 import { useImage } from '../../../foundation/hooks/useImage';
 import { Color, Radius, Space, Typography } from '../../../foundation/styles/variables';
 import { useBook } from '../../book/hooks/useBook';
-
 const _Wrapper = styled.li`
   width: 100%;
 `;
@@ -92,7 +91,7 @@ const RankingCard: React.FC<Props> = ({ bookId }) => {
               <Text color={Color.Secondary} typography={Typography.NORMAL14} weight="bold">
                 この漫画を読む
               </Text>
-              <SvgIcon color={Color.Secondary} height={32} type="NavigateNext" width={32} />
+              <NavigateNext style={{ color: Color.Secondary, height: 32, width: 32 }} />
             </Flex>
           </Box>
         </Flex>
--- workspaces/app/src/pages/BookDetailPage/internal/FavButton.tsx
+++ workspaces/app/src/pages/BookDetailPage/internal/FavButton.tsx
@@ -1,6 +1,6 @@
+import { Favorite, FavoriteBorder } from '@mui/icons-material';
 import styled from 'styled-components';
 
-import { SvgIcon } from '../../../features/icons/components/SvgIcon';
 import { Color, Space } from '../../../foundation/styles/variables';
 
 const _Button = styled.button<{ $outlined: boolean }>`
@@ -22,18 +22,15 @@ type Props = {
 } & JSX.IntrinsicElements['button'];
 
 export const FavButton: React.FC<Props> = ({ enabled, onClick }) => {
+  const IconTag = enabled ? Favorite : FavoriteBorder;
+
   return (
     <_Button
       $outlined={!enabled}
       aria-label={enabled ? 'お気に入りを解除する' : 'お気に入りに追加する'}
       onClick={onClick}
     >
-      <SvgIcon
-        color={enabled ? Color.Favorite : Color.MONO_40}
-        height={24}
-        type={enabled ? 'Favorite' : 'FavoriteBorder'}
-        width={24}
-      />
+      <IconTag style={{ fill: enabled ? Color.Favorite : Color.MONO_40, height: 24, width: 24 }} />
     </_Button>
   );
 };
