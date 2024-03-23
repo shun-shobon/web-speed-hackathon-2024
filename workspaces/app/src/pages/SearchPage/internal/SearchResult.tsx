149c2324715e33d934d999832d2aadc070b91d36
NISHIZAWA Shuntaro <me@s2n.tech>
2024-03-24 03:51:35 +0900

    Reapply "作品検索の絞り込みをサーバーサイドで行うようにした"
    This reverts commit 3768dd8fe6adbec7cad9ac00a05c89a6753e6393.

--- workspaces/app/src/pages/SearchPage/internal/SearchResult.tsx
+++ workspaces/app/src/pages/SearchPage/internal/SearchResult.tsx
@@ -1,46 +1,32 @@
-import { Suspense, useMemo } from 'react';
-
-import type { GetBookListResponse } from '@wsh-2024/schema/src/api/books/GetBookListResponse';
+import { memo } from 'react';
 
 import { BookListItem } from '../../../features/book/components/BookListItem';
-import { Flex } from '../../../foundation/components/Flex';
+import { useBookList } from '../../../features/book/hooks/useBookList';
 import { Text } from '../../../foundation/components/Text';
 import { Color, Typography } from '../../../foundation/styles/variables';
-import { isContains } from '../../../lib/filter/isContains';
 
 type Props = {
-  books: GetBookListResponse;
   keyword: string;
 };
 
-export const SearchResult: React.FC<Props> = ({ books, keyword }) => {
-  const relatedBooks = useMemo(() => {
-    if (keyword === '') {
-      return books;
-    }
-    return books.filter((book) => {
-      return isContains({ query: keyword, target: book.name }) || isContains({ query: keyword, target: book.nameRuby });
-    });
-  }, [books, keyword]);
+export const SearchResult: React.FC<Props> = memo(({ keyword }: Props) => {
+  const { data: books } = useBookList({ query: { limit: 30, name: keyword } });
+
+  if (books.length === 0) {
+    return (
+      <Text color={Color.MONO_100} typography={Typography.NORMAL14}>
+        関連作品は見つかりませんでした
+      </Text>
+    );
+  }
 
   return (
-    <Flex align="center" as="ul" direction="column" justify="center">
-      <Suspense
-        fallback={
-          <Text color={Color.MONO_100} typography={Typography.NORMAL14}>
-            「{keyword}」を検索中...
-          </Text>
-        }
-      >
-        {relatedBooks.map((book) => (
-          <BookListItem key={book.id} book={book} />
-        ))}
-        {relatedBooks.length === 0 && (
-          <Text color={Color.MONO_100} typography={Typography.NORMAL14}>
-            関連作品は見つかりませんでした
-          </Text>
-        )}
-      </Suspense>
-    </Flex>
+    <>
+      {books.map((book) => (
+        <BookListItem key={book.id} book={book} />
+      ))}
+    </>
   );
-};
+});
+
+SearchResult.displayName = 'SearchResult';
