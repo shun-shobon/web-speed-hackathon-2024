03ed9bba184fbfe9a7e215ffa0a32690a8d8173a
NISHIZAWA Shuntaro <me@s2n.tech>
2024-03-23 15:10:53 +0900

    jqueryを削除

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -1,6 +1,5 @@
 // import './side-effects';
 
-import $ from 'jquery';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SWRConfig } from 'swr';
@@ -15,20 +14,19 @@ const main = async () => {
   await registerServiceWorker();
   await preloadImages();
 
-  $(document).ready(() => {
-    if (window.location.pathname.startsWith('/admin')) {
-      ReactDOM.createRoot($('#root').get(0)!).render(<AdminApp />);
-    } else {
-      ReactDOM.hydrateRoot(
-        $('#root').get(0)!,
-        <SWRConfig value={{ revalidateIfStale: true, revalidateOnFocus: false, revalidateOnReconnect: false }}>
-          <BrowserRouter>
-            <ClientApp />
-          </BrowserRouter>
-        </SWRConfig>,
-      );
-    }
-  });
+  const container = document.querySelector('#root');
+  if (window.location.pathname.startsWith('/admin')) {
+    ReactDOM.createRoot(container!).render(<AdminApp />);
+  } else {
+    ReactDOM.hydrateRoot(
+      container!,
+      <SWRConfig value={{ revalidateIfStale: true, revalidateOnFocus: false, revalidateOnReconnect: false }}>
+        <BrowserRouter>
+          <ClientApp />
+        </BrowserRouter>
+      </SWRConfig>,
+    );
+  }
 };
 
 main().catch(console.error);
