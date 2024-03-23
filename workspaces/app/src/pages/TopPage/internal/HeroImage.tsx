a9300bc4e81a2a11a2c01d425663667015b07d8c
NISHIZAWA Shuntaro <me@s2n.tech>
2024-03-23 21:41:55 +0900

    ヒーローの画像サイズを増やす

--- workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
+++ workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
@@ -6,5 +6,20 @@ const _Image = styled.img`
 `;
 
 export const HeroImage: React.FC = () => {
-  return <_Image alt="Cyber TOON" decoding="async" loading="eager" src="/assets/hero.webp" />;
+  return (
+    <_Image
+      alt="Cyber TOON"
+      decoding="async"
+      loading="eager"
+      sizes="(max-width: 1024px) 100vw, 1024px"
+      src="/assets/hero-1024.webp"
+      srcSet={[
+        '/assets/hero-256.webp 256w',
+        '/assets/hero-384.webp 384w',
+        '/assets/hero-512.webp 512w',
+        '/assets/hero-768.webp 768w',
+        '/assets/hero-1024.webp 1024w',
+      ].join(',')}
+    />
+  );
 };
